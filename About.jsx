88411b775c9b3b5c281ee0e3b241d4f33f64b031
Vedavyas Rokalaboina <vedavyasrokalaboina1@gmail.com>
2026-02-26 17:04:11 +0000

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>An aeronautical engineering graduate from the University of South Wales with strong experience in aerodynamic analysis, simulation and engineering design. Based in the UK.</p>
+    </section>
+  );
+}
+
+export default About;
