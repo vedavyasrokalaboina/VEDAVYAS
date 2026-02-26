3fe70114a2c9ae3c59352879f20d33571cc42640
Vedavyas Rokalaboina <vedavyasrokalaboina1@gmail.com>
2026-02-26 17:02:57 +0000

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
