16797271f48de9f3eacdcaa6a0d181fcf02698df
Vedavyas Rokalaboina <vedavyasrokalaboina1@gmail.com>
2026-02-26 17:05:32 +0000

    Create Contact.jsx

---
+++ Contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Email: vedavyasrokalaboin1@gmail.com</p>
+      <p>LinkedIn: linkedin.com/in/vedavyas-rokalaboina/</p>
+    </section>
+  );
+}
+
+export default Contact;
