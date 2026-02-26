b005e6d7feee0518f2a09a5eb3e40880a67b4075
Vedavyas Rokalaboina <vedavyasrokalaboina1@gmail.com>
2026-02-26 17:03:36 +0000

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Vedavyas Rokalaboina</h1>
+      <p>Aeronautical Engineering | CFD • FEA • Aircraft Design</p>
+      <p>United Kingdom</p>
+    </header>
+  );
+}
+
+export default Header;
