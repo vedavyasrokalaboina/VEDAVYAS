d9305d968939b687fa9bde6da7644f970c698b03
Vedavyas Rokalaboina <vedavyasrokalaboina1@gmail.com>
2026-02-26 17:04:36 +0000

    Create Skills.jsx

---
+++ Skills.jsx
@@ -0,0 +1,17 @@
+function Skills() {
+  return (
+    <section id="skills">
+      <h2>Skills</h2>
+      <ul>
+        <li>ANSYS</li>
+        <li>SolidWorks</li>
+        <li>MATLAB</li>
+        <li>CFD</li>
+        <li>FEA</li>
+        <li>CAD Modelling</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
